8e650b35f30acf7357fbcb135581fb6a2a754b1c
Ruslan Nurnazarov <ruslan7692@gmail.com>
2025-07-15 18:01:51 +0400

    fix some httpexchange stuff

--- src/main/java/com/example/javaproductservice/controller/ProductController.java
+++ src/main/java/com/example/javaproductservice/controller/ProductController.java
@@ -6,8 +6,8 @@ import com.example.javaproductservice.controller.dto.response.CreateProductRespo
 import com.example.javaproductservice.controller.dto.response.GetProductResponseDto;
 import com.example.javaproductservice.controller.dto.response.UpdateProductResponseDto;
 import com.example.javaproductservice.service.ProductService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
+//import io.swagger.v3.oas.annotations.Operation;
+//import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -24,37 +24,37 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/products")
 @RequiredArgsConstructor
-@Tag(name = "Product Service", description = "API для работы с продуктами")
+//@Tag(name = "Product Service", description = "API для работы с продуктами")
 public class ProductController {
     private final ProductService productService;
 
     @GetMapping()
-    @Operation(summary = "Чтение всех продуктов")
+//    @Operation(summary = "Чтение всех продуктов")
     public ResponseEntity<List<GetProductResponseDto>> getAllProducts() {
         return ResponseEntity.ok(productService.findAllProducts());
     }
 
     @GetMapping("/{productId}")
-    @Operation(summary = "Чтение конкретного продукта")
+//    @Operation(summary = "Чтение конкретного продукта")
     public ResponseEntity<GetProductResponseDto> getProductById(@PathVariable("productId") Long productId) {
         return ResponseEntity.ok(productService.findProductById(productId));
     }
 
     @PostMapping()
-    @Operation(summary = "Создание нового продукта")
+//    @Operation(summary = "Создание нового продукта")
     public ResponseEntity<CreateProductResponseDto> createProduct(@RequestBody CreateProductRequestDto requestDto) {
         return ResponseEntity.ok(productService.createProduct(requestDto));
     }
 
     @PutMapping("/{productId}")
-    @Operation(summary = "Изменение продукта")
+//    @Operation(summary = "Изменение продукта")
     public ResponseEntity<UpdateProductResponseDto> updateProduct(@PathVariable("productId") Long productId,
                                                                 @RequestBody UpdateProductRequestDto requestDto) {
         return ResponseEntity.ok(productService.updateProduct(productId, requestDto));
     }
 
     @DeleteMapping("/{productId}")
-    @Operation(summary = "Удаление продукта")
+//    @Operation(summary = "Удаление продукта")
     public ResponseEntity<Void> deleteProduct(@PathVariable("productId") Long productId) {
         productService.deleteProduct(productId);
         return ResponseEntity.noContent().build();
